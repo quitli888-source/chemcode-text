746c85bd5972af3a9a9fda355c27c5ac222c9b60
Chen Yusheng <chenw2759@gmail.com>
2026-06-09 20:05:11 +0800

    add chemycode/src/views/index.ts

---
+++ chemycode/src/views/index.ts
@@ -0,0 +1,8 @@
+// ====== View barrel =====
+import './chat-view';
+import './task-detail-view';
+import './knowledge-view';
+import './database-view';
+import './skills-view';
+import './settings-view';
+import './usage-view';
